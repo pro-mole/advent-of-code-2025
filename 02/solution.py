e816b66d3af836dc0fb30e39fbe67252de99698f
Bruno Guedes <guedesav@gmail.com>
2025-12-02 23:44:51 -0500

    Day 02 - Part 2
    Aside from me not realizing the max range for the size of the pattern should be half of the string
    and not the square root of it, it was pretty seamless :V

--- 02/solution.py
+++ 02/solution.py
@@ -1,4 +1,7 @@
 #!/usr/local/bin/python3
+import math
+
+DEBUG = True
 
 seq = []
 with open("input", "r") as input:
@@ -17,3 +20,34 @@ for _range in seq:
                 invalid_sum += id
 
 print(f"Part 1 - Solution: {invalid_sum}")
+
+def has_repeats(number_str):
+    length = len(number_str)
+    if length == 1:
+        return False
+    
+    for size in range(1, (length//2)+1):
+        if len(number_str) % size != 0:
+            continue
+
+        pattern = number_str[:size]
+        repeats = True
+        for i in range(1, length // size):
+            if pattern != number_str[size*i:size*(i+1)]:
+                repeats = False
+                break
+        
+        if repeats: return True
+
+    return False
+
+invalid_sum = 0
+
+for _range in seq:
+    _from, _to = _range.split("-")
+    for id in range(int(_from), int(_to)+1):
+        if has_repeats(str(id)):
+            if DEBUG: print(id)
+            invalid_sum += id
+
+print(f"Part 2 - Solution: {invalid_sum}")
