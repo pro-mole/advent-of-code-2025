7145eb3cb1724da8b152a380bf886b698f8a5626
Bruno Guedes <guedesav@gmail.com>
2025-12-04 00:30:31 -0500

    Day 04 - Part 2
    Part 1, but iterate, remove the rolls and count the removed rolls; repeat until no more rolls can be removed;
    The frustrating thing here is that Python does not let you change string's characters in place :P

--- 04/solution.py
+++ 04/solution.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python3
-DEBUG = True
+DEBUG = False
 
 map = []
 with open("./input", "r") as input:
@@ -34,3 +34,44 @@ for line in map:
     y += 1
 
 print(f"Part 1 Solution: {rolls}")
+
+rolls = 0
+drolls = []
+
+w = len(map[0])
+h = len(map)
+
+while(True):
+    y = 0
+    for line in map:
+        x = 0
+        for char in line:
+            if char == "@":
+                adj = 0
+                for dy in [-1,0,1]:
+                    for dx in [-1,0,1]:
+                        if dx == 0 and dy == 0:
+                            continue
+                        if x + dx < 0 or x + dx >= w or y + dy < 0 or y + dy >= h:
+                            continue
+                        if map[y+dy][x+dx] == "@":
+                            adj += 1
+                        if adj >= 4:
+                            break
+                if adj < 4:
+                    drolls.append((x,y))
+                    if DEBUG: print(f"{x},{y} : {adj}")
+            x += 1
+        y += 1
+    
+    if len(drolls) == 0: break
+    rolls += len(drolls)
+    if DEBUG: print(drolls)
+    for (rollx,rolly) in drolls:
+        map[rolly] = map[rolly][:rollx] + "." + map[rolly][rollx+1:]
+    drolls = []
+
+    if DEBUG:
+        for line in map: print(line)
+
+print(f"Part 2 Solution: {rolls}")
