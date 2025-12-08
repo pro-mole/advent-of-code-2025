c657cfee8bad81b4d48cfc4ebb550f7cdef673ee
Bruno Guedes <guedesav@gmail.com>
2025-12-08 01:38:32 -0500

    Day 06 Part 2
    A rare case where re-reading the input makes it easier to compute
    the second solution; the nature of the grid makes it fairly easy
    to group the numbers vertically, and the right-to-left direction
    makes not difference at all

--- 06/solution.py
+++ 06/solution.py
@@ -25,3 +25,34 @@ for i in range(len(ops)):
         print(f"{op.join([str(numberline[i]) for numberline in numbers])} = {result}")
 
 print(f"Part 1 Solution: {sum_total}")
+
+numbers = []
+ops = []
+
+with open("input", "r") as input:
+    grid = input.readlines()
+
+    for x in range(len(grid[-1])):
+        op = grid[-1][x]
+        if op != ' ':
+            if DEBUG and len(ops) > 0:
+                print(f"{numbers[-1]} {ops[-1]}")
+            ops.append(op)
+            numbers.append([])
+        
+        number = ""
+        for y in range(len(grid)-1):
+            number += grid[y][x]
+        if number.strip().isnumeric():
+            numbers[-1].append(int(number))
+
+sum_total = 0
+
+for i in range(len(ops)-1):
+    op = ops[i]
+    result = 0 if op == '+' else 1
+    for number in numbers[i]:
+        result = (result + number) if op == '+' else (result * number)
+    sum_total += result
+
+print(f"Part 2 Solution: {sum_total}")
