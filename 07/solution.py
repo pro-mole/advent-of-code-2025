8ba06c4ced2431a59933798018104d7c99a702f2
Bruno Guedes <guedesav@gmail.com>
2025-12-09 23:27:41 -0500

    Day 07 Part 2
    I took way too long to realize why the example for the timelines was giving out
    such a low result XD
    
    After that, we're basically down to dynamic programming and, of course, we gotta
    memoize it :P

--- 07/solution.py
+++ 07/solution.py
@@ -1,12 +1,13 @@
 #!/usr/local/bin/python3
 DEBUG = False
+INPUT_FILENAME = "input"
 
 beams = set()
 splitters = []
 if DEBUG:
     line_size = 0
 
-with open("input", "r") as input:
+with open(INPUT_FILENAME, "r") as input:
     start_line = input.readline()
     beams.add(start_line.find('S'))
     if DEBUG: line_size = len(start_line)-1
@@ -39,3 +40,26 @@ for round in splitters:
         print(line)
 
 print(f"Part 1 Solution: {splits}")
+
+# Part 2 seems to be Dynamic Programming...
+# Recursive implementation
+def timeline_splits(round_table, start_row, start_pos, memo={}):
+    if not (start_row, start_pos) in memo:
+        if start_row >= len(round_table)-1:
+            memo[(start_row, start_pos)] = 1
+        else:
+            next_row = round_table[start_row+1]
+            if start_pos in next_row:
+                memo[(start_row, start_pos)] = timeline_splits(round_table, start_row + 1 , start_pos - 1, memo) + timeline_splits(round_table, start_row + 1 , start_pos + 1, memo)
+            else:
+                memo[(start_row, start_pos)] = timeline_splits(round_table, start_row + 1, start_pos, memo)
+        
+    return memo[(start_row, start_pos)]
+
+timelines = 0
+
+with open(INPUT_FILENAME, "r") as input:
+    start_line = input.readline()
+    timelines = timeline_splits(splitters, 0, start_line.find('S'))
+
+print(f"Part 2 Solution: {timelines}")
